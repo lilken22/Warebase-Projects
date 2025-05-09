a5244da02794b22970541e5f21f11ff8160f8421
charleeblaq <blaqwayschannel@gmail.com>
2025-05-09 15:57:11 +0100

    updates url

--- my-app/src/redux/actionTypes.jsx
+++ my-app/src/redux/actionTypes.jsx
@@ -2,8 +2,8 @@
 import { Navigate } from "react-router-dom";
 import { getItemFromLocalStorage } from "../utitlity/storage";
 
-// export const URL = "https://dev-api.warebase.com.ng/api/v1";
-export const URL = "http://localhost:3535/api/v1";
+export const URL = "https://dev-api.warebase.com.ng/api/v1";
+// export const URL = "http://localhost:3535/api/v1";
 
 
 export const ProtectedRoute = ({ children }) => {
